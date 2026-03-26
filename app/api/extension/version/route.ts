9c454da5fabc0c7b8ce13eec2a0ac9fd287d0132
vladimirsapundzic85 <vladimirsapundzic@gmail.com>
2026-03-26 09:55:49 +0100

    Update route.ts

--- app/api/extension/version/route.ts
+++ app/api/extension/version/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({
     ok: true,
-    latest_extension_version: "0.1.2",
+    latest_extension_version: "0.1.1",
     minimum_extension_version: "0.1.1",
     update_url: "https://vetasist.carrd.co/",
     message: "Dostupna je nova verzija ekstenzije."
